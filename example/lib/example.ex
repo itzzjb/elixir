5533ba10878936ffdf75d1ccd47684f90654af67
itzzjb <desilvabethmin@gmail.com>
2026-02-12 15:06:01 +0530

    Fix the syntax issue found

--- example/lib/example.ex
+++ example/lib/example.ex
@@ -5,17 +5,17 @@ defmodule Example do
 
   @doc """
   Hello world.
-
+  """
   
   # need to compile before running the code via `mix compile` 
-  # function that prints a value 
+  # function that prints a value ( returns "world" and :ok  via iex) 
   # is both visible via `iex -S mix` -> `Example.hello`  + `mix run -e "Example.hello"`
   def hello do
     IO.puts(:world)
   end
   
   ## visible via `iex -S mix` -> `Example.hello` but not visible via `mix run -e "Example.hello"`
-  # function that returns a value 
+  # function that returns a value ( returns :helloworld via iex ) 
   def helloworld do 
     :helloworld
   end 
