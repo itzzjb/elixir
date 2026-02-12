377cd540f66384e058235828154699a9b111f364
itzzjb <desilvabethmin@gmail.com>
2026-02-12 16:20:41 +0530

    Adding a default runnig function using mix.exs

--- example/lib/example.ex
+++ example/lib/example.ex
@@ -1,28 +1,15 @@
 defmodule Example do
-  @moduledoc """
-  Documentation for `Example`.
-  """
 
-  @doc """
-  Hello world.
-  """
-  
-  # need to compile before running the code via `mix compile` 
-  # function that prints a value ( returns "world" and :ok  via iex) 
-  # is both visible via `iex -S mix` -> `Example.hello`  + `mix run -e "Example.hello"`
-  def hello do
-    IO.puts(:world)
+  use Application
+
+  def start(_type, _args) do
+    # code
+    IO.puts(Example.hello())
+    Supervisor.start_link([], strategy: :one_for_one)
   end
-  
-  ## visible via `iex -S mix` -> `Example.hello` but not visible via `mix run -e "Example.hello"`
-  # function that returns a value ( returns :helloworld via iex ) 
-  def helloworld do 
-    :helloworld
-  end 
-end
 
-# These functioned called here are only being ran in the compile time 
-# So only when running `mix compile` or `mix run` for the fisttime they will be executed (when there are no changes it doesn't need to compile again) 
-Example.hello
-Example.helloworld 
-IO.puts("This only runs in the compile time")
+  def hello do
+    :hello
+  end 
+  
+end 
